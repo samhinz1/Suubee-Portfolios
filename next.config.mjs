15189e83b4ae76929873f20f45348ddd6398d947
Sam Hinz <samhinz@live.com>
2025-05-07 14:20:45 +1000

    hopefully with fixes

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/Suubee-Portfolios' : '',
+  // Correctly configure for GitHub Pages
+  basePath: '/Suubee-Portfolios',
+  assetPrefix: '/Suubee-Portfolios',
   // Remove trailing slash for GitHub Pages
   trailingSlash: false,
-  // Fixes issues with GitHub Pages
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Suubee-Portfolios' : '',
+  // Disable image optimization for static export
+  images: {
+    unoptimized: true,
+    domains: [],
+    remotePatterns: [],
+  },
 }
 
 export default nextConfig
