5252272aad0bb355b8ae155030a360d2ff1be5a2
Sam Hinz <samhinz@live.com>
2025-05-07 14:14:39 +1000

    Fix GitHub Pages deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,11 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/suubee-portfolios' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Suubee-Portfolios' : '',
   // Remove trailing slash for GitHub Pages
   trailingSlash: false,
+  // Fixes issues with GitHub Pages
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Suubee-Portfolios' : '',
 }
 
 export default nextConfig
